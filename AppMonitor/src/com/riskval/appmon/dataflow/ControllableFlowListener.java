ebbd0364af238be630e477af49d07175a25e300a
gogog22510 <gogog22510@gmail.com>
2015-07-17 10:34:43 -0400

    move folder

--- AppMonitor/src/com/riskval/appmon/dataflow/ControllableFlowListener.java
+++
@@ -1,5 +0,0 @@
-package com.riskval.appmon.dataflow;
-
-public interface ControllableFlowListener {
-	public void fireFlowInterrupted(SequentialFlow flow);
-}
--- AppMonitor/src/com/riskval/appmon/dataflow/Monitorable.java
+++
@@ -1,5 +0,0 @@
-package com.riskval.appmon.dataflow;
-
-public interface Monitorable {
-	public void fireStateChanged(String newState);
-}
