67adbede054edbdd6b2c5277b926791ab8fec4c6
gogog22510 <gogog22510@gmail.com>
2015-08-28 09:38:33 -0400

    enhance the transformer interface

--- FMon/src/gogog22510/appmon/assist/AbstractTransformer.java
+++ FMon/src/gogog22510/appmon/assist/AbstractTransformer.java
@@ -3,7 +3,10 @@ package gogog22510.appmon.assist;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javassist.CannotCompileException;
 import javassist.ClassPool;
@@ -12,7 +15,28 @@ import javassist.CtClass;
 import javassist.NotFoundException;
 
 public abstract class AbstractTransformer implements ClassFileTransformer {
-	protected Set<String> redefinedClass, redefinedMethod;
+	private Map<String, List<String>> redefinedMap;
+	
+	public AbstractTransformer() {
+		redefinedMap = new HashMap<String, List<String>>();
+	}
+	
+	protected void addNewMethod(String clazzName, String methodName) {
+		if(clazzName == null || methodName == null) return;
+		if(redefinedMap.containsKey(clazzName)) {
+			List<String> methods = redefinedMap.get(clazzName);
+			if(methods == null) {
+				methods = new ArrayList<String>();
+				redefinedMap.put(clazzName, methods);
+			}
+			if(!methods.contains(methodName))
+				methods.add(methodName);
+		} else {
+			List<String> methods = new ArrayList<String>();
+			methods.add(methodName);
+			redefinedMap.put(clazzName, methods);
+		}
+	}
 
 	@Override
 	public byte[] transform(ClassLoader loader, String className,
@@ -21,13 +45,13 @@ public abstract class AbstractTransformer implements ClassFileTransformer {
 		return transformClass(classBeingRedefined, classfileBuffer);
 	}
 	
-	protected byte[] transformClass(Class classToTransform, byte[] b) {
+	protected byte[] transformClass(Class<?> classToTransform, byte[] b) {
 		ClassPool pool = ClassPool.getDefault();
 		CtClass cl = null;
 		try {
 			cl = pool.makeClass(new java.io.ByteArrayInputStream(b));
 			// if contains classes
-			if(redefinedClass.contains(cl.getName())) {
+			if(redefinedMap.containsKey(cl.getName())) {
 				CtBehavior[] methods = cl.getDeclaredBehaviors();
 				for (int i = 0; i < methods.length; i++) {
 					if (methods[i].isEmpty() == false) {
@@ -47,10 +71,13 @@ public abstract class AbstractTransformer implements ClassFileTransformer {
 	}
 	
 	protected void changeMethod(CtBehavior method) throws NotFoundException, CannotCompileException {
-		if(redefinedMethod.contains(method.getName())) {
+		List<String> methods = redefinedMap.get(method.getClass().getName());
+		if(methods.contains(method.getName())) {
 			changeMethodImpl(method);
 		}
 	}
+	
+	protected abstract void initializeMapping(Map<String, List<String>> redefinedMap);
 	
 	protected abstract void changeMethodImpl(CtBehavior method) throws NotFoundException, CannotCompileException;
 }
